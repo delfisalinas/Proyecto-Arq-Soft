14c688c1768509bfea100ab8d6cb67f9f36681d3
SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>
2024-07-13 21:29:58 -0300

    Update ProtectedRoute.js
    Signed-off-by: SantiagoTricherri <165184361+SantiagoTricherri@users.noreply.github.com>

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  const token = localStorage.getItem('token'); 
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
+    const token = localStorage.getItem('token');
+
+    return token ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
